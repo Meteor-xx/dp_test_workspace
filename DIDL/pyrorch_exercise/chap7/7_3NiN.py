847ac7becb5b717955df0063a16938f8caef3356
Meteordevourer <470411626@qq.com>
2022-09-01 20:35:54 +0800

    [ADD]DIDL Chap6_3 -> Chap7 completed

---
+++ DIDL/pyrorch_exercise/chap7/7_3NiN.py
@@ -0,0 +1,111 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
+
+
+net = nn.Sequential(
+    nin_block(1, 96, kernel_size=11, strides=4, padding=0),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(96, 256, kernel_size=5, strides=1, padding=2),
+    nn.MaxPool2d(3, stride=2),
+    nin_block(256, 384, kernel_size=3, strides=1, padding=1),
+    nn.MaxPool2d(3, stride=2),
+    nn.Dropout(0.5),
+    # 标签类别数是10
+    nin_block(384, 10, kernel_size=3, strides=1, padding=1),
+    nn.AdaptiveAvgPool2d((1, 1)),
+    # 将四维的输出转成二维的输出，其形状为(批量大小,10)
+    nn.Flatten())
+
+# X = torch.rand(size=(1, 1, 224, 224))
+# for layer in net:
+#     X = layer(X)
+#     print(layer.__class__.__name__,'output shape:\t', X.shape)
+
+# Sequential output shape:	 torch.Size([1, 96, 54, 54])
+# MaxPool2d output shape:	 torch.Size([1, 96, 26, 26])
+# Sequential output shape:	 torch.Size([1, 256, 26, 26])
+# MaxPool2d output shape:	 torch.Size([1, 256, 12, 12])
+# Sequential output shape:	 torch.Size([1, 384, 12, 12])
+# MaxPool2d output shape:	 torch.Size([1, 384, 5, 5])
+# Dropout output shape:	 torch.Size([1, 384, 5, 5])
+# Sequential output shape:	 torch.Size([1, 10, 5, 5])
+# AdaptiveAvgPool2d output shape:	 torch.Size([1, 10, 1, 1])
+# Flatten output shape:	 torch.Size([1, 10])
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """使用GPU计算模型在数据集上的精度"""
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = d2l.Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(d2l.accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_ch6(net, train_iter, test_iter, num_epochs, lr, device, animator):
+    """用GPU训练模型(在第六章定义)"""
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+    net.apply(init_weights)
+    print('training on', device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    timer, num_batches = d2l.Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        # 训练损失之和，训练准确率之和，样本数
+        metric = d2l.Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (train_l, train_acc, None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+          f'on {str(device)}')
+
+
+lr, num_epochs, batch_size = 0.1, 10, 128
+animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                        legend=['train loss', 'train acc', 'test acc'])
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu(), animator)
+animator.show()
+# loss 0.385, train acc 0.859, test acc 0.852
+# 1935.8 examples/sec on cuda:0
